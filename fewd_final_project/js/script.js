36c08bbae319a3367d9cefd1e97fcc39b8496897
Abdirisak206 <bdimohamud@gmail.com>
2022-06-21 16:31:00 -0700

    Test check

---
+++ fewd_final_project/js/script.js
@@ -0,0 +1,52 @@
+const pageTwoButton = document.getElementById('Page');
+const secondMenu = document.getElementById('second-menu');
+
+pageTwoButton.addEventListener("click", displaySecondPage);
+
+function displaySecondPage(){
+    secondMenu.classList.add("show-page");
+}
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("Holybutton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
