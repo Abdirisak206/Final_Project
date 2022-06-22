2e6f22b5fb78844d2c5ab1580dd4b866b37185ec
Abdirisak206 <bdimohamud@gmail.com>
2022-06-22 15:08:33 -0700

    final

--- fewd_final_project/js/script.js
+++ fewd_final_project/js/script.js
@@ -7,26 +7,23 @@ function displaySecondPage(){
     secondMenu.classList.add("show-page");
 }
 
-// Get the modal
+
 const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+
 const btn = document.getElementById("Holybutton");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -37,7 +34,7 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
